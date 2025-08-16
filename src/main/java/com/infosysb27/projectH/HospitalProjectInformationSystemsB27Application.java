6e22feae4fd00a0f2fcc5525f86e21150f78fa95
Daffa Tarigan <daffatarigan69@gmail.com>
2025-08-16 12:20:09 +0700

    Initialization BE

---
+++ src/main/java/com/infosysb27/projectH/HospitalProjectInformationSystemsB27Application.java
@@ -0,0 +1,13 @@
+package com.infosysb27.projectH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalProjectInformationSystemsB27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalProjectInformationSystemsB27Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosysb27/projectH/HospitalProjectInformationSystemsB27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosysb27.projectH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalProjectInformationSystemsB27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
