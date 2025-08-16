b2fe8b10c2a0877d5d41468ff4040696a693a657
Daffa Tarigan <daffatarigan69@gmail.com>
2025-08-16 17:39:57 +0700

    sql addition

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,88 @@
+create table Patient(
+    idPatient serial primary key,
+    patientName varchar(255) not null,
+    dateOfBirth date not null,
+    phone varchar(15) not null,
+    constraint phoneCheck check (phone ~ '^\+?[1-9][0-9]{7,14}$'),
+    address text not null,
+    email varchar(50) not null,
+    password text not null,
+    gender char(1) not null,
+    allergy text);
+
+create table Insurance(
+    idInsurance serial primary key,
+    name varchar(50) not null,
+    contactPerson varchar(50) not null,
+    phoneNumber varchar(15) not null,
+    constraint phoneCheck check (phoneNumber ~ '021[0-9]{6,8}$'));
+
+create table DetailInsurance(
+    idInsurance serial not null,
+    idPatient serial not null,
+    primary key (idInsurance, idPatient),
+    foreign key (idInsurance) references Insurance(idInsurance) on delete cascade,
+    foreign key (idPatient) references Patient(idPatient) on delete cascade);
+
+create table Doctor(
+    idDoctor serial primary key,
+    namaDoctor varchar(255) not null,
+    photo text, 
+    tanggalLahir date not null,
+    phone varchar(15) not null,
+    constraint phoneCheck check (phone ~ '^\+?[1-9][0-9]{7,14}$'),
+    address text not null);
+
+create table Specialty(
+    idSpecialty serial primary key,
+    specialty varchar(100) primary key);
+
+create table DoctorSpecialty(
+    idDoctor serial not null,
+    idSpecialty serial not null,
+    primary key (idDoctor, idSpecialty),
+    foreign key (idDoctor) references Doctor(idDoctor) on delete cascade,
+    foreign key (idSpecialty) references Specialty(idSpecialty) on delete cascade);
+
+create table DetailDoctor(
+    idDoctor serial not null,
+    experience smallint not null check (experience > 0),
+    price integer not null check (price > 0),
+    onLeave boolean not null,
+    foreign key (idDoctor) references Doctor(idDoctor) on delete cascade);
+
+create table Hospital(
+    idHospital serial primary key,
+    location text not null,
+    photo text,
+    capacity smallint not null check (capacity > 0),
+    services jsonb not null default '{}'::jsonb,
+    facilities jsonb not null default '{}':: jsonb);
+
+create table DoctorShift(
+    idHospital serial not null,
+    idDoctor serial not null,
+    shift timestamp not null);
+
+create table Admin(
+    idAdmin serial not null,
+    namaAdmin varchar(100) not null,
+    address text not null,
+    dateOfBirth date not null,
+    email varchar(50) not null,
+    password text not null,
+    gender char(1) not null);
+
+create table Review(
+    idReview serial primary key,
+    rating smallint not null,
+    description text not null
+    reviewType varchar(8) not null check (reviewType in ('Doctor', 'Hospital')),
+    reviewedId serial not null);
+
+create table Appointment(
+    idAppointment serial primary key,
+    appointmentDate timestamp default current_date;,
+    consultationType varchar(16) not null check (consultationType in ('Konultasi Umum', 'Medical Check Up','Kontrol Ulang')),
+    complaints jsonb not null,
+    doctorNotes text);
