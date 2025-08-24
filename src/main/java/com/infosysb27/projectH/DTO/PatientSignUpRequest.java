8be09aa93a21797355edb67e6a740e09a423d6e7
Arya <aryaswastika312@gmail.com>
2025-08-24 13:38:41 +0700

    Model getter setter for DTO

--- src/main/java/com/infosysb27/projectH/DTO/PatientSignUpRequest.java
+++ src/main/java/com/infosysb27/projectH/DTO/PatientSignUpRequest.java
@@ -12,4 +12,67 @@ public class PatientSignUpRequest {
     private String phone;
     private String allergy;
     
+public String getName(){
+    return name;
+}
+
+public void setName(String newName){
+    this.name = newName;
+}
+
+public LocalDate getDateOfBirth(){
+    return dateOfBirth;
+}
+
+public void setDateOfBirth(String newDateOfBirth){
+    this.dateOfBirth = newDateOfBirth;
+}
+
+public char getGender(){
+    return gender;
+}
+
+public void setGender(String newGender){
+    this.gender = newGender;
+}
+
+public String getEmail(){
+    return email;
+}
+
+public void setEmail(String newEmail){
+    this.email = newEmail;
+}
+
+public String getPassword(){
+    return password;
+}
+
+public void setPassword(String newPassword){
+    this.password = newPassword;
+}
+
+public String getAddress(){
+    return address;
+}
+
+public void setAddress(String newAddress){
+    this.address = newAddress;
+}
+
+public String getPhone(){
+    return phone;
+}
+
+public void setPhone(String newPhone){
+    this.phone = newPhone;
+}
+
+public String getAllergy(){
+    return allergy;
+}
+
+public void setAllergy(String newAllergy){
+    this.allergy = newAllergy;
+}
 }
